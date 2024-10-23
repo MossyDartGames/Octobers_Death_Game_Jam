f4f0237d22f930f5aa4aee2fd5d26cd2e5de27bd
jackielu3 <jackielu3@gmail.com>
2024-10-23 16:07:15 -0700

    uwu

--- Assets/Scripts/Player/Attack/Gun.cs
+++ Assets/Scripts/Player/Attack/Gun.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] Transform gunTip;
     [SerializeField] GameObject bullet;
-    
+    [SerializeField] float bulletSpeed = 20f;
+
     private void Shoot()
     {
-    //    Instantiate(bullet, gunTip, );
+        GameObject b = Instantiate(bullet, gunTip.position, gunTip.rotation);
+        Rigidbody rb = b.GetComponent<Rigidbody>();
+        if (rb != null )
+        {
+            rb.velocity = gunTip.forward * bulletSpeed;
+        }
+        
     }
 
 }
